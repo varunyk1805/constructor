94ac9e03f8f0d32e8dd465199ce1ad9cc2dea9c7
varunyk1805 <varunyk1805@gmail.com>
2022-02-22 20:12:42 +0200

    Додано навішування івенту через цикл

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const form = document.querySelector('.form');
-const imageInput = document.querySelector('#input-image');
+const inputs = document.querySelectorAll('.input');
 const imageError = document.querySelector('.error');
 const imageThumb = document.querySelector('.thumb');
-const titleInput = document.querySelector('#input-title');
 const titleError = document.querySelector('#error-title');
-const linkInput = document.querySelector('#input-link');
 const linkError = document.querySelector('#error-link');
-const textarea = document.querySelector('#textarea');
 const textareaError = document.querySelector('#error-textarea');
 const submitBtn = document.querySelector('.submit');
 const box = document.querySelector('#box');
@@ -18,17 +15,13 @@ const blocks = [];
 let page = 1;
 let countMaxBlocksInPage = PER_PAGE * page;
 
+let file;
 let imageURL;
 let titleValue;
 let linkValue;
 let textValue;
 
 const renderImageInForm = () => {
-    const file = imageInput.files[0];
-    if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
-        imageThumb.innerHTML = '';
-        return;
-    };
     imageURL = window.URL.createObjectURL(file);
     imageThumb.innerHTML = `<img src="${imageURL}" width="190" />`;
 };
@@ -53,78 +46,77 @@ const renderBlocks = blocks => {
 };
 
 const handlleValidationInput = input => {
-    switch (input.id) {
-        case 'input-image':
-            if (!imageInput.value) {
+    switch (input.dataset.input) {
+        case 'image':
+            if (!input.value) {
                 imageError.textContent = 'Нужно выбрать изображения следующих форматов: .png, .jpeg';
-                return false;
+                return;
             };
 
-            const file = imageInput.files[0];
+            file = input.files[0];
             if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
                 imageError.textContent = 'Разрешено выбрать изображения следующих форматов: .png, .jpeg';
                 imageError.classList.add('active');
-                return false;
+                imageThumb.innerHTML = '';
+                return;
             };
         
             imageError.textContent = '';
             imageError.classList.remove('active');
-            return true;
+            renderImageInForm();
+            break;
         
-        case 'input-title':
+        case 'title':
             if (!input.value) {
                 titleError.textContent = 'Поле ввода не может быть пустым';
-                return false;
-            }; 
+                return;
+            };
             if (!input.value.match(/^[А-Яа-яЁёІіЇїЄєҐґ\s]+$/)) {
                 titleError.textContent = 'Только кириллические символы';
                 titleError.classList.add('active');
-                return false;
+                return;
             };
+            titleValue = input.value;
             titleError.textContent = '';
             titleError.classList.remove('active');
-            return true;  
-        
-        
-        case 'input-link':
+            break;
+            
+        case 'link':
             if (!input.value) {
                 linkError.textContent = 'Поле ввода не может быть пустым';
-                return false;
-            }; 
-
+                return;
+            };
+                
+            linkValue = input.value;
             linkError.textContent = '';
             linkError.classList.remove('active');
-            return true;  
-        
-        
-        case 'textarea':
+            break;
+            
+        case 'text':
             if (!input.value) {
                 textareaError.textContent = 'Поле ввода не может быть пустым';
-                return false;
-            }; 
+                return;
+            };
             if (!input.value.match(/^[А-Яа-яЁёІіЇїЄєҐґ\s]+$/)) {
                 textareaError.textContent = 'Только кириллические символы';
                 textareaError.classList.add('active');
-                return false;
+                return;
             };
+            textValue = input.value;
             textareaError.textContent = '';
             textareaError.classList.remove('active');
-            return true;  
-    };      
+            break;
+        
+        default:
+            return;
+    };
 };
 
 const handlleSubmit = event => {
     event.preventDefault();
-    
-    if (!handlleValidationInput(imageInput, imageError)) return;
-    if (!handlleValidationInput(titleInput, titleError)) return;
-    if (!handlleValidationInput(linkInput, linkError)) return;
-    if (!handlleValidationInput(textarea, textareaError)) return;
 
-    titleValue = titleInput.value;
-    linkValue = linkInput.value;
-    textValue = textarea.value;
-    
+    inputs.forEach(input =>  handlleValidationInput(input));
+
     blocks.push({ imageURL, titleValue, textValue, linkValue });
     
     imageThumb.innerHTML = '';
@@ -152,12 +144,6 @@ const handlleLoadMore = () => {
     loadMoreBtn.classList.add('is-none');
 };
 
-imageInput.addEventListener('input', () => {
-    handlleValidationInput(imageInput);
-    renderImageInForm();
-});
-titleInput.addEventListener("input", () => handlleValidationInput(titleInput));
-linkInput.addEventListener("input", () => handlleValidationInput(linkInput));
-textarea.addEventListener("input", () => handlleValidationInput(textarea));
+inputs.forEach(input => input.addEventListener("input", event => handlleValidationInput(event.target)));
 submitBtn.addEventListener('click', handlleSubmit);
 loadMoreBtn.addEventListener('click', handlleLoadMore);
